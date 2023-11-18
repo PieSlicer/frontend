1732d5ed9a2b127bbb1d8ed6beee38f9bd1800c6
flo-3 <florianelf@gmail.com>
2023-11-18 22:17:47 +0100

    feat: add mint button

--- src/pages/marketplace/[contractAddress]/[tokenId].tsx
+++ src/pages/marketplace/[contractAddress]/[tokenId].tsx
@@ -1,21 +1,30 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 import Image from "next/image";
 import Link from 'next/link';
 
 import { useNFTMetadata } from "@/hooks/nft";
+import { usePrepareContractWrite, useContractWrite, useWaitForTransaction } from 'wagmi';
+import { formatSCAddress } from '@/utils/scUtils';
+import NFTABI from '@/abis/psnft.json';
+import { parseEther } from 'viem';
+import { useAccount } from "wagmi";
+
 
 import { nounsURL } from '@/hooks/profilePicture';
 import Layout from "@/components/layout/Layout";
-import { Spinner, Profile, LeftArrowSVG } from '@ensdomains/thorin';
+import { Spinner, Profile, LeftArrowSVG, Button, Banner } from '@ensdomains/thorin';
 import Logo from 'public/logo.jpeg';
+import { useAuth } from '@/components/auth';
 
 
 const TokenPage = () => {
   const router = useRouter();
   const rootCid = process.env.NEXT_PUBLIC_SLICER_CID as string; //TODO: query from contract
   const { contractAddress, tokenId } = router.query;
+  //const { isConnected } = useAuth();
+  const { isConnected } = useAccount();
+
 
   const { metadata: nft, loading, owners } = useNFTMetadata({ cid: rootCid, tokenId, contractAddress });
   const imageUrl = (nft && nft?.image) ? nft?.image?.replace('ipfs://', 'https://ipfs.io/ipfs/') : '';
@@ -25,6 +34,47 @@ const TokenPage = () => {
       <div className="flex items-center space-x-2 my-10">
         <Link href="/marketplace"> <LeftArrowSVG/> </Link>
         <span>Marketplace / {nft?.collection} / {nft?.name}</span>
+      </div>
+    )
+  }
+
+  const { config } = usePrepareContractWrite({
+    address: formatSCAddress(contractAddress as string),
+    abi: NFTABI.abi,
+    functionName: 'mint',
+    args: [tokenId],
+    enabled: true,
+    value: parseEther('0.001'),
+    onSuccess(data) {
+      console.log('onSuccess usePrepareContractWrite', data)
+    },
+    onError(error) {
+      console.log('onError usePrepareContractWrite', error)
+    },
+  })
+  const { data, write } = useContractWrite(config)
+ 
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  const BuyButton = () => {
+    if (!write) {
+      return null
+    }
+    return (
+      <div>
+        <Button disabled={!write || isLoading} onClick={() => write()}>
+          {isLoading ? 'Minting...' : 'Mint'}
+        </Button>
+        {isSuccess && (
+          <div>
+            Successfully minted your NFT!
+            <div>
+              <a href={`https://sepolia.etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+            </div>
+          </div>
+        )}
       </div>
     )
   }
@@ -65,6 +115,18 @@ const TokenPage = () => {
             </div>
           }
           <p className="">{nft?.description} </p>
+          {
+            (!owners || owners.length === 0) && (
+              isConnected ?
+                <BuyButton />
+              :
+              <div className='pt-12'>
+                <Banner alert="warning" title="">
+                  Please login to buy this NFT
+                </Banner>
+              </div>
+            )
+          }
         </div>
       </div>
     </Layout>
